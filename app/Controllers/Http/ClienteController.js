4c4cf76dfe2d287beeeb5543907cba54d5f995c9
Thomas Andrioli Leick <thomasaleick@gmail.com>
2024-02-28 23:06:43 -0300

    feat: add implementation of POST /clientes/ to create new client

--- app/Controllers/Http/ClienteController.js
+++ app/Controllers/Http/ClienteController.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Cliente = use('App/Models/Cliente')
+const Endereco = use('App/Models/Endereco')
+const Telefone = use('App/Models/Telefone')
 
     class ClienteController {
         async index ({ response }) {
@@ -28,6 +30,30 @@ const Cliente = use('App/Models/Cliente')
         return response.status(500).json({ message: 'Failed to fetch clients.' })
         }
     }
+
+    async store({ request, response }) {
+    try {
+      // Extrair os dados do cliente, endereço e telefone do corpo da requisição
+      const data = request.only(['nome', 'cpf'])
+      const enderecoData = request.only(['rua', 'cidade', 'estado', 'cep'])
+      const telefoneData = request.only(['numero'])
+
+      // Criar um novo cliente no banco de dados
+      const cliente = await Cliente.create(data)
+
+      // Criar um novo endereço associado ao cliente
+      const endereco = await Endereco.create({ ...enderecoData, cliente_id: cliente.id })
+
+      // Criar um novo telefone associado ao cliente
+      const telefone = await Telefone.create({ ...telefoneData, cliente_id: cliente.id })
+
+      // Retornar o cliente, endereço e telefone recém-criados
+      return response.status(201).json({ message: 'Cliente adicionado com sucesso.', cliente, endereco, telefone })
+    } catch (error) {
+      console.error(error)
+      return response.status(500).json({ message: 'Erro ao adicionar cliente.' })
+    }
+  }
 }
 
 module.exports = ClienteController
