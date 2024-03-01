3300eab3459b2b79e789e5f7ee8b2b7fbddb4b02
Thomas Andrioli Leick <thomasaleick@gmail.com>
2024-03-01 17:35:13 -0300

    refactor: Use transaction in store method of ClienteController

--- app/Controllers/Http/ClienteController.js
+++ app/Controllers/Http/ClienteController.js
@@ -7,7 +7,7 @@ const Venda = use('App/Models/Venda')
 const Database = use('Database')
 
     class ClienteController {
-        async index ({ response }) {
+      async index ({ response }) {
         try {
         // Pegar a lista de clientes no banco de dados
         const clients = await Cliente.query().orderBy('id').fetch()
@@ -31,31 +31,35 @@ const Database = use('Database')
         console.error(error)
         return response.status(500).json({ message: 'Failed to fetch clients.' })
         }
+      } 
+
+      async store({ request, response }) {
+        const trx = await Database.beginTransaction()
+        try {
+          // Extrair os dados do cliente, endereço e telefone do corpo da requisição
+          const data = request.only(['nome', 'cpf'])
+          const enderecoData = request.only(['rua', 'cidade', 'estado', 'cep'])
+          const telefoneData = request.only(['numero'])
+
+          // Criar um novo cliente no banco de dados
+          const cliente = await Cliente.create(data)
+
+          // Criar um novo endereço associado ao cliente
+          const endereco = await Endereco.create({ ...enderecoData, cliente_id: cliente.id })
+
+          // Criar um novo telefone associado ao cliente
+          const telefone = await Telefone.create({ ...telefoneData, cliente_id: cliente.id })
+
+          await trx.commit()
+
+          // Retornar o cliente, endereço e telefone recém-criados
+          return response.status(201).json({ message: 'Cliente adicionado com sucesso.', cliente, endereco, telefone })
+        } catch (error) {
+          await trx.rollback()
+          console.error(error)
+          return response.status(500).json({ message: 'Erro ao adicionar cliente.' })
+        }
     }
-
-    async store({ request, response }) {
-    try {
-      // Extrair os dados do cliente, endereço e telefone do corpo da requisição
-      const data = request.only(['nome', 'cpf'])
-      const enderecoData = request.only(['rua', 'cidade', 'estado', 'cep'])
-      const telefoneData = request.only(['numero'])
-
-      // Criar um novo cliente no banco de dados
-      const cliente = await Cliente.create(data)
-
-      // Criar um novo endereço associado ao cliente
-      const endereco = await Endereco.create({ ...enderecoData, cliente_id: cliente.id })
-
-      // Criar um novo telefone associado ao cliente
-      const telefone = await Telefone.create({ ...telefoneData, cliente_id: cliente.id })
-
-      // Retornar o cliente, endereço e telefone recém-criados
-      return response.status(201).json({ message: 'Cliente adicionado com sucesso.', cliente, endereco, telefone })
-    } catch (error) {
-      console.error(error)
-      return response.status(500).json({ message: 'Erro ao adicionar cliente.' })
-    }
-  }
 
   async show({ params, request, response }) {
     try {
