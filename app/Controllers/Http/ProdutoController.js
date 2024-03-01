bf1a5724df6a29ccccff5b22e0f90d235ff3f2c9
Thomas Andrioli Leick <thomasaleick@gmail.com>
2024-03-01 17:43:25 -0300

    feat: add implementation of GET /produtos/ to fetch all products

--- app/Controllers/Http/ProdutoController.js
+++ app/Controllers/Http/ProdutoController.js
@@ -1,6 +1,35 @@
 'use strict'
 
+const Produto = use('App/Models/Produto')
+
 class ProdutoController {
+
+    async index({ response }) {
+        try {
+            // Pegar a lista de produtos no banco de dados, ordenados alfabeticamente
+            const produtos = await Produto.query().orderBy('nome').fetch()
+
+            // Verificar se a lista está vazia
+            if (produtos.rows.length === 0) {
+            return response.status(204).json({ message: 'Nenhum produto encontrado.' })
+            }
+
+            // Retorna apenas os dados essenciais de cada produto
+            const simplifiedProdutos = produtos.map(produto => {
+            return {
+                id: produto.id,
+                nome: produto.nome,
+                descricao: produto.descricao,
+                // Adicione outros campos essenciais conforme necessário
+            }
+            })
+
+            return response.status(200).json({ produtos: simplifiedProdutos })
+        } catch (error) {
+            console.error(error)
+            return response.status(500).json({ message: 'Falha ao buscar produtos.' })
+        }
+    }
 }
 
 module.exports = ProdutoController
